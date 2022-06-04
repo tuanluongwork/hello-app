a7d2622748ef9b1e804f88e2711ca221e5e6c34c
tuanluongwork <tuanluongwork@gmail.com>
2022-06-04 13:22:14 +0700

    Convert Celsius to Fahrenheit degrees

--- main.py
+++ main.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def index():
     return "Congratulations, it's a web app!"
 
+@app.route("/<int:celsius>")
+def fahrenheit_from(celsius):
+    """Convert Celsius to Fahrenheit degrees."""
+    fahrenheit = float(celsius) * 9 / 5 + 32
+    fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+    return str(fahrenheit)
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
